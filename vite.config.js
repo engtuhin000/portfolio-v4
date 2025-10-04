aee32591b847580b41add72593f15c62a238d4bb
engtuhin000 <eng.tuhin000@gmail.com>
2025-10-04 20:54:36 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
+  root: '.',              // 👈 root directory explicitly set
+  base: './',             // 👈 relative base path for Netlify
+  build: {
+    outDir: 'dist',       // 👈 ensure output folder name
+  },
 })
